863a6b1e37a694661459426f6bc0b4d376aef131
nikitaolegovich <xdoku@yandex.ru>
2022-11-13 23:21:21 +0500

    add brain_gcd, gcd_logic

---
+++ brain_games/logic/gcd_logic.py
@@ -0,0 +1,39 @@
+from brain_games.cli import welcome_user
+from random import randint
+from prompt import string
+
+
+def gcd(first_number, second_number):
+    min_number = min(first_number, second_number)
+    max_divider = 0
+
+    for divider in range(1, min_number):
+        if first_number % divider == 0 and second_number % divider == 0:
+            max_divider = max(max_divider, divider)
+
+    return str(max_divider)
+
+
+def logic():
+    name = welcome_user()
+    count = 0
+
+    print('Find the greatest common divisor of given numbers.')
+
+    while count != 3:
+        first_number = randint(0, 100)
+        second_number = randint(0, 100)
+        max_common_divider = gcd(first_number, second_number)
+
+        answer = string(f"Question: {first_number} {second_number}, Answer: ")
+        print(f'Your answer: {answer}')
+
+        if max_common_divider == answer:
+            print('Correct!')
+            count += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{max_common_divider}'. \nLet's"
+                  f"try again, {name}")
+            count = 0
+
+    print(f'Congratulations, {name}!')
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from brain_games.logic.gcd_logic import logic
+
+
+def main():
+    logic()
+
+
+if __name__ == '__main__':
+    main()
